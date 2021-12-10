83378429d1c1c8e0cfac7f2b65e2b65213d6287e
Bob <y1713050991@gmail.com>
2021-12-10 18:53:35 +0800

    docs(design pattern): sync creational pattern

--- DesignPattern/src/main/java/com/android/designpattern/创建型/建造者模式/CommonDialog.java
+++ DesignPattern/src/main/java/com/android/designpattern/创建型/建造者模式/CommonDialog.java
@@ -8,92 +8,49 @@ public class CommonDialog {
         this.mParams = params;
     }
 
+    public void setTitleText(String title) {
+        mParams.titleText = title;
+    }
+
+    public void setMessageText(String message) {
+        mParams.messageText = message;
+    }
+
     public void show() {
-        //...
-        //setTitle(mParams.titleText)
-        //...
+        //set view...
     }
 
     public static class Builder {
 
-        enum TYPE {
-            TYPE_NORMAL, TYPE_ONLY_CONFIRM, TYPE_ONLY_MESSAGE
-        }
-
         protected Params mParams = new Params();
-
-        private final Builder builder;
-
-        public Builder() {
-            this(TYPE.TYPE_NORMAL);
-        }
-
-        public Builder(TYPE type) {
-            builder = new Builder();
-            switch (type) {
-                case TYPE_NORMAL:
-                    buildNormal();
-                    break;
-                case TYPE_ONLY_CONFIRM:
-                    buildOnlyConfirm();
-                    break;
-                case TYPE_ONLY_MESSAGE:
-                    buildOnlyMessage();
-                    break;
-            }
-        }
-
-        private void buildNormal() {
-            mParams.cancelable = true;
-            mParams.onlyMessageText = false;
-            mParams.onlyConfirmButton = false;
-            mParams.confirmText = "Confirm";
-            mParams.cancelText = "Cancel";
-        }
-
-        private void buildOnlyConfirm() {
-            buildNormal();
-        }
-
-        private void buildOnlyMessage() {
-        }
 
         public Builder setTitleText(String title) {
             mParams.titleText = title;
-            return builder;
+            return this;
         }
 
         public Builder setMessageText(String message) {
             mParams.messageText = message;
-            return builder;
+            return this;
         }
 
         public Builder setConfirmText(String confirm) {
             mParams.confirmText = confirm;
-            return builder;
+            return this;
         }
 
         public Builder setCancelText(String cancel) {
             mParams.cancelText = cancel;
-            return builder;
-        }
-
-        public Builder setCancelable(boolean cancelable) {
-            mParams.cancelable = cancelable;
-            return builder;
-        }
-
-        public Builder setOnlyMessageText(boolean onlyMessageText) {
-            mParams.onlyMessageText = onlyMessageText;
-            return builder;
-        }
-
-        public Builder setOnlyConfirmButton(boolean onlyConfirmButton) {
-            mParams.onlyConfirmButton = onlyConfirmButton;
-            return builder;
+            return this;
         }
 
         public CommonDialog create() {
+            if (mParams.confirmText == null) mParams.confirmText = "ok";
+            return new CommonDialog(mParams);
+        }
+
+        public CommonDialog createOnlyConfirm() {
+            mParams.cancelText = null;
             return new CommonDialog(mParams);
         }
 
@@ -106,10 +63,6 @@ public class CommonDialog {
 
         private String confirmText;
         private String cancelText;
-
-        private boolean cancelable;
-        private boolean onlyMessageText;
-        private boolean onlyConfirmButton;
 
     }
 
