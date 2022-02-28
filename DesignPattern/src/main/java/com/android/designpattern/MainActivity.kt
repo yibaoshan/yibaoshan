5ba61afda58a2b8c5bad6e75d879462a56e164fb
Bob <1713050991@qq.com>
2022-02-28 23:24:23 +0800

    docs(design pattern): sync the observer pattern

--- DesignPattern/src/main/java/com/android/designpattern/MainActivity.kt
+++ DesignPattern/src/main/java/com/android/designpattern/MainActivity.kt
@@ -1,9 +1,15 @@
 package com.android.designpattern
 
+import android.content.BroadcastReceiver
+import android.content.ContentValues.TAG
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.android.designpattern.behavioral.cor.broadcast.OrderlyBroadcastActivity
 import com.android.designpattern.structural.proxy.binder.client.BinderClientActivity
 
@@ -17,11 +23,24 @@ class MainActivity : AppCompatActivity() {
 
     private fun init() {
         findViewById<Button>(R.id.btn_launch_binder).setOnClickListener {
+            LocalBroadcastManager.getInstance(this).sendBroadcast(Intent("IntentFilter_001"))
             startActivity(Intent(this, BinderClientActivity::class.java))
         }
         findViewById<Button>(R.id.btn_launch_orderly_broadcast).setOnClickListener {
             startActivity(Intent(this, OrderlyBroadcastActivity::class.java))
         }
+        val intentFilter1 = IntentFilter("IntentFilter_001");
+        LocalBroadcastManager.getInstance(this).registerReceiver(object : BroadcastReceiver(){
+            override fun onReceive(p0: Context?, p1: Intent?) {
+                Log.e(TAG, "onReceive: 1")
+                Thread.sleep(3000)
+            }
+        },intentFilter1)
+        LocalBroadcastManager.getInstance(this).registerReceiver(object : BroadcastReceiver(){
+            override fun onReceive(p0: Context?, p1: Intent?) {
+                Log.e(TAG, "onReceive: 2")
+            }
+        },intentFilter1)
     }
 
 }
