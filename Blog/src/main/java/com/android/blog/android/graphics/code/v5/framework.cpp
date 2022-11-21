2ad3d78e8db284e90da64dd3232031991e4da3e7
Bob <y1713050991@gmail.com>
2022-11-21 17:58:37 +0800

    docs(blog): 同步 Android 事件分发笔记

--- Blog/src/main/java/com/android/blog/android/graphics/code/v5/framework.cpp
+++ Blog/src/main/java/com/android/blog/android/graphics/code/v5/framework.cpp
@@ -2,6 +2,60 @@
 // Created by Bob on 2022/9/13.
 //
 
+//drivers/input/input.c
+//核心层，为驱动层提供设备注册，和操作接口，是 input 子系统的核心类
+class input {
+
+    //注册一个 input 设备到内核
+    int input_register_device(input_dev *dev){
+    }
+
+    //从内核注销掉一个 input 设备
+    void input_unregister_device(input_dev *dev){
+    }
+
+    int input_attach_handler(input_dev *dev, input_handler *handler){
+        handler->evdev_connect();
+    }
+
+    //设备上报一个 input 事件，input 子系统对其处理
+    void input_event(input_dev *dev, type, code, value){
+        input_handle_event(dev, type, code, value);
+    }
+
+    void input_handle_event(input_dev *dev, type, code, value){
+    }
+}
+
+//drivers/input/evdev.c
+//事件层，和用户空间进行交互，提供事件监听功能。类似的类还有 mousedev.c 、keyboard.c
+class evdev {
+
+    //选择一个事件设备进行连接
+    int evdev_connect(input_handler *handler, input_dev *dev, input_device_id *id){
+    }
+
+    //取消监听
+    void evdev_disconnect(input_handle *handle){
+    }
+}
+
+//-----------------------------------------------------------------------Framework inputflinger----------------------------------------------------------------
+
+/frameworks/native/services/inputflinger/InputReader.cpp
+class InputReader {
+
+    void InputReader::loopOnce() {
+        int count = mEventHub->getEvents();//读消息，有消息返回，没消息阻塞到epoll()
+        if(count) processEventsLocked();//分发原始事件
+    }
+
+    void processEventsLocked(RawEvent ev){
+        InputDevice:process();//解析原始事件，得到 NotifyMotionArgs 类型消息
+        InputDispatcher::notifyMotion();//分发输入事件
+    }
+}
+
 /frameworks/base/core/java/android/view/ViewRootImpl.java
 class ViewRootImpl {
 
@@ -114,19 +168,7 @@ class InputManager {
     }
 }
 
-/frameworks/native/services/inputflinger/InputReader.cpp
-class InputReader {
 
-    void InputReader::loopOnce() {
-        int count = mEventHub->getEvents();//读消息，有消息返回，没消息阻塞到epoll()
-        if(count) processEventsLocked();//分发原始事件
-    }
-
-    void processEventsLocked(RawEvent ev){
-        InputDevice:process();//解析原始事件，得到 NotifyMotionArgs 类型消息
-        InputDispatcher::notifyMotion();//分发输入事件
-    }
-}
 
 /frameworks/native/services/inputflinger/InputDispatcher.cpp
 class InputDispatcher {
@@ -183,42 +225,3 @@ class InputDispatcher {
     }
 }
 
-//-----------------------------------------------------------------------Linux分界线----------------------------------------------------------------
-
-//drivers/input/input.c
-//核心层，为驱动层提供设备注册，和操作接口，是 input 子系统的核心类
-class input {
-
-    //注册一个 input 设备到内核
-    int input_register_device(input_dev *dev){
-    }
-
-    //从内核注销掉一个 input 设备
-    void input_unregister_device(input_dev *dev){
-    }
-
-    int input_attach_handler(input_dev *dev, input_handler *handler){
-        handler->evdev_connect();
-    }
-
-    //设备上报一个 input 事件，input 子系统对其处理
-    void input_event(input_dev *dev, type, code, value){
-        input_handle_event(dev, type, code, value);
-    }
-
-    void input_handle_event(input_dev *dev, type, code, value){
-    }
-}
-
-//drivers/input/evdev.c
-//事件层，和用户空间进行交互，提供事件监听功能。类似的类还有 mousedev.c 、keyboard.c
-class evdev {
-
-    //选择一个事件设备进行连接
-    int evdev_connect(input_handler *handler, input_dev *dev, input_device_id *id){
-    }
-
-    //取消监听
-    void evdev_disconnect(input_handle *handle){
-    }
-}
