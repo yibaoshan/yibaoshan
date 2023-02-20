6b628c19e9d7b275cb5854a7f36e29aace98cc38
bob <1713050991@qq.com>
2023-02-20 23:03:46 +0800

    docs(review): 同步 bitmap 内存测试 demo

--- app/src/main/java/com/android/blackboard/DrawableMemoryActivity.java
+++ app/src/main/java/com/android/blackboard/DrawableMemoryActivity.java
@@ -1,6 +1,8 @@
 package com.android.blackboard;
 
+import android.content.res.Resources;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
@@ -29,6 +31,9 @@ public class DrawableMemoryActivity extends AppCompatActivity {
     public void onWindowFocusChanged(boolean hasFocus) {
         super.onWindowFocusChanged(hasFocus);
         printBitmapSize(img);
+        Bitmap bitmap = decodeSampledBitmapFromResource(getResources(), R.mipmap.test, img.getWidth(), img.getHeight());
+        img.setImageBitmap(bitmap);
+        printBitmapSize(img);
     }
 
     private void printBitmapSize(ImageView imageView) {
@@ -40,6 +45,30 @@ public class DrawableMemoryActivity extends AppCompatActivity {
         } else {
             Log.e(TAG, "Drawable is null !");
         }
+    }
+
+    // 让加载的Bitmap和view宽度或者高度一样
+    public static Bitmap decodeSampledBitmapFromResource(Resources res, int resId,
+                                                         int reqWidth, int reqHeight) {
+        // 第一次解析将inJustDecodeBounds设置为true，来获取图片大小
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(res, resId, options);
+        // 调用上面定义的方法计算inSampleSize值
+        if(options.outHeight/reqHeight >options.outWidth/reqWidth){
+            options.inDensity = options.outHeight;
+            options.inTargetDensity =reqHeight;
+        }else{
+            options.inDensity = options.outWidth;
+            options.inTargetDensity = reqWidth;
+        }
+        // 指定色彩模式，不带透明度指定为 565 ，内存占用小一半
+        options.inPreferredConfig = Bitmap.Config.RGB_565;
+        // 指定压缩比
+        options.inSampleSize = 4;
+        // 使用获取到的inSampleSize值再次解析图片
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeResource(res, resId, options);
     }
 
 }
