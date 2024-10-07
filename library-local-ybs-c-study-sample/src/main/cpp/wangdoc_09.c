b410d6d504f6c6fe5af7e1d4758ea89faa9d7f76
yibaoshan <yibaoshan@foxmail.com>
2024-10-07 11:33:11 +0800

    feat: c array section completed

--- library-local-ybs-c-study-sample/src/main/cpp/wangdoc_09.c
+++ library-local-ybs-c-study-sample/src/main/cpp/wangdoc_09.c
@@ -1,5 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+
+void p1() {
+    int scores[100] = {[2] = 29, [9] = 7, [14] = 48};;
+    scores[0] = 13;
+    scores[99] = 42;
+    printf("scores: %zu\n", sizeof(scores));
+}
+
+// 数组指针的加减法 #
+void p6() {
+    int arr[5] = {11, 22, 33, 44, 55};
+    for (int i = 0; i < 5; i++) {
+        printf("%d\n", *(arr + i));
+    }
+}
 
 int main(void) {
+    system("clear");
+    system("ps");
+    printf("\npart of c array\n\n");
+    p1();
+    p6();
+    printf("c 语言需显式传递数组长度");
     return 0;
 }
